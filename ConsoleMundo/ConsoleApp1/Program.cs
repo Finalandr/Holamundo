fd273a532fa6a66c904f8f3a75a4b095eef44f48
finalandr <20163524@itla.edu.do>
2018-11-07 05:00:08 -0400

    Hola Mundo

---
+++ ConsoleMundo/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Anthony Perez 2016-3524");
+            Console.ReadKey();
+        }
+    }
+}
